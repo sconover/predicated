aad14d8ab500a6b664eb1cc83c04f5096c8b4ec8
Steve Conover <sconover@gmail.com>
2010-06-27 11:23:38 -0700

    finishing test conversion

--- test/suite.rb
+++ test/suite.rb
@@ -1,3 +1,3 @@
 #simple way to make sure requires are isolated
-result = Dir["test/**/*_spec.rb"].collect{|spec_file| system("ruby #{spec_file}") }.uniq == [true]
+result = Dir["test/**/*_test.rb"].collect{|test_file| system("ruby #{test_file}") }.uniq == [true]
 exit(result ? 0 : 1)
