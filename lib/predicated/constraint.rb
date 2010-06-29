dc955814202b83ebcc2f18787d3fa10b6f437fe0
Steve Conover <sconover@gmail.com>
2010-06-28 23:10:25 -0700

    reworking constraints again.  constraint result.

--- lib/predicated/constraint.rb
+++ lib/predicated/constraint.rb
@@ -4,20 +4,61 @@ module Predicated
       @constraints = []
     end
     
-    def add(check_proc, selectors=[:all])
-      @constraints << {
-        :selectors => selectors,
-        :check_proc => check_proc
-      }
+    def add(constraint)
+      @constraints << constraint
       self
     end
     
     def check(whole_predicate)
+      result = ConstraintCheckResult.new
+      
       @constraints.collect do |constraint|
-        whole_predicate.select(*constraint[:selectors]).collect do |predicate, ancestors|
-          constraint[:check_proc].call(predicate, ancestors)
-        end.uniq == [true]
-      end.flatten.uniq == [true]
+        whole_predicate.select(*constraint.selectors).collect do |predicate, ancestors|
+          if ! constraint.check(predicate, ancestors)
+            result.violation(constraint, predicate)
+          end
+        end
+      end
+      
+      result
+    end
+    
+    def ==(other)
+      @constraints == other.instance_variable_get("@constraints".to_sym)
+    end
+  end
+  
+  class Constraint
+    attr_reader :name, :selectors
+    def initialize(args)
+      @name = args[:name]
+      @selectors = args[:selectors] || [:all]
+      @check_that = args[:check_that]
+    end
+    
+    def check(predicate, ancestors)
+      @check_that.call(predicate, ancestors)
+    end
+    
+    def ==(other)
+      @name == other.name && @selectors == other.selectors
+    end
+  end
+  
+  class ConstraintCheckResult
+    attr_reader :violations
+    def initialize
+      @violations = {}
+    end
+    
+    def pass?
+      @violations.empty?
+    end
+    
+    def violation(constraint, predicate)
+      @violations[constraint] ||= []
+      @violations[constraint] << predicate
+      self
     end
   end
 end
--- test/constraint_test.rb
+++ test/constraint_test.rb
@@ -6,29 +6,72 @@ include Predicated
 
 apropos %{constraints are rules about the content and structure of predicates.
           a predicate might violate a constraint} do
-  test "constraints - apply to each predicate" do
-    constraints = 
-      Constraints.new.
-        add(proc{|predicate, ancestors| predicate.right!=2}, [Operation])
-    
-    assert{ constraints.check(Predicate{Eq(1,1)}) == true }
-    deny  { constraints.check(Predicate{Eq(1,2)}) == true }
-    
-    assert{ constraints.check(Predicate{And(Eq(1,1), Eq(1,3))}) == true }
-    deny  { constraints.check(Predicate{And(Eq(1,1), Eq(1,2))}) == true }
+            
+  before do
+    @value_not_equal_to_two =
+      Constraint.new(:name => "Value can't be two",
+                     :selectors => [Operation],
+                     :check_that => proc{|predicate, ancestors| predicate.right!=2})
+                     
+    @not_more_than_two_levels_deep =
+      Constraint.new(:name => "Limited to two levels deep",
+                     :check_that => proc{|predicate, ancestors| ancestors.length<=2})
   end
   
-  test "constraints - apply each to each predicate" do
+  test "apply to each predicate - simple" do
+    constraints = Constraints.new.add(@value_not_equal_to_two)
+    
+    assert{ constraints.check(Predicate{Eq(1,1)}).pass? }
+    deny  { constraints.check(Predicate{Eq(1,2)}).pass? }
+    
+    assert{ constraints.check(Predicate{And(Eq(1,1), Eq(1,3))}).pass? }
+    deny  { constraints.check(Predicate{And(Eq(1,1), Eq(1,2))}).pass? }
+  end
+  
+  test "apply each to each predicate - many constraints" do
     constraints = 
       Constraints.new.
-        add(proc{|predicate, ancestors| predicate.right!=2}, [Operation]).
-        add(proc{|predicate, ancestors| ancestors.length<=2})
+        add(@value_not_equal_to_two).
+        add(@not_more_than_two_levels_deep)
 
-    assert{ constraints.check(Predicate{And(Eq(1,1), Eq(3,3))}) == true }
-    deny  { constraints.check(Predicate{And(Eq(1,1), Eq(2,2))}) == true }
+    assert{ constraints.check(Predicate{And(Eq(1,1), Eq(3,3))}).pass? }
+    deny  { constraints.check(Predicate{And(Eq(1,1), Eq(2,2))}).pass? }
     
-    assert{ constraints.check(Predicate{And(Eq(1,1), Eq(3,3))}) == true }
-    deny  { constraints.check(Predicate{Or(Or(And(Eq(1,1), Eq(3,3)), Eq(4,4)), Eq(5,5))}) == true }
+    assert{ constraints.check(Predicate{And(Eq(1,1), Eq(3,3))}).pass? }
+    deny  { constraints.check(Predicate{Or(Or(And(Eq(1,1), Eq(3,3)), Eq(4,4)), Eq(5,5))}).pass? }
   end
+
+  test "equality" do
+    one = Constraint.new(:name => "Value can't be two",
+                         :selectors => [Operation],
+                         :check_that => proc{|predicate, ancestors| predicate.right!=2})
+    two = Constraint.new(:name => "Value can't be two",
+                         :selectors => [Operation],
+                         :check_that => proc{|predicate, ancestors| predicate.right!=2})
+    three = Constraint.new(:name => "Some other constraint",
+                           :check_that => proc{|predicate, ancestors| false})
+                           
+    assert{ one == two }
+    deny  { one == three }
+  end
+
+  test %{result contains information about whether the checks passed, 
+         which constraints were violated, 
+         along with the offending predicates} do
+    constraints = Constraints.new.add(@value_not_equal_to_two)
+    
+    result = constraints.check(Predicate{Eq(1,1)})
+    assert{ result.pass? }
+    assert{ result.violations == {} }
+    
+    result = constraints.check(Predicate{And(Eq(1,1), And(Eq(2,2), Eq(3,2)))})
+    deny  { result.pass? }
+    assert{ 
+      result.violations == {
+        @value_not_equal_to_two => [Equal.new(2,2), Equal.new(3,2)]
+      } 
+    }
+  end
+
   
 end
