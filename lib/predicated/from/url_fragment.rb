4ce63159555a4ae3a16da44ac12eda5057ac08d0
Steve Conover <sconover@gmail.com>
2010-06-30 23:14:24 -0700

    going to a from/to method style, symetrically, for now

--- lib/predicated/from/url_fragment.rb
+++ lib/predicated/from/url_fragment.rb
@@ -1,11 +1,9 @@
 require "predicated/from/url_fragment_parser"
 
 module Predicated
-  module From
-    class UrlFragment
-      def convert(url_fragment_string)
-        TreetopUrlFragmentParser.new.parse(url_fragment_string).to_predicate
-      end
+  module Predicate
+    def self.from_url_fragment(url_fragment_string)
+      TreetopUrlFragmentParser.new.parse(url_fragment_string).to_predicate
     end
   end
 end
--- test/from/url_fragment_test.rb
+++ test/from/url_fragment_test.rb
@@ -6,33 +6,30 @@ include Predicated
 apropos "parse url fragments and convert them into predicates" do
 
   apropos "basic operations" do
-    before do
-      @converter = From::UrlFragment.new
-    end
-    
+
     test "simple signs" do
-      assert { @converter.convert("a=1") == Predicate{ Eq("a","1") } }
-      assert { @converter.convert("a<1") == Predicate{ Lt("a","1") } }
-      assert { @converter.convert("a>1") == Predicate{ Gt("a","1") } }
-      assert { @converter.convert("a<=1") == Predicate{ Lte("a","1") } }
-      assert { @converter.convert("a>=1") == Predicate{ Gte("a","1") } }
+      assert { Predicate.from_url_fragment("a=1") == Predicate{ Eq("a","1") } }
+      assert { Predicate.from_url_fragment("a<1") == Predicate{ Lt("a","1") } }
+      assert { Predicate.from_url_fragment("a>1") == Predicate{ Gt("a","1") } }
+      assert { Predicate.from_url_fragment("a<=1") == Predicate{ Lte("a","1") } }
+      assert { Predicate.from_url_fragment("a>=1") == Predicate{ Gte("a","1") } }
     end
 
     test "simple and + or" do
-      assert { @converter.convert("a=1&b=2") == Predicate{ And(Eq("a","1"),Eq("b","2")) } }
-      assert { @converter.convert("a=1|b=2") == Predicate{ Or(Eq("a","1"),Eq("b","2")) } }
+      assert { Predicate.from_url_fragment("a=1&b=2") == Predicate{ And(Eq("a","1"),Eq("b","2")) } }
+      assert { Predicate.from_url_fragment("a=1|b=2") == Predicate{ Or(Eq("a","1"),Eq("b","2")) } }
     end
 
     test "complex and + or" do
-      assert { @converter.convert("a=1&b=2|c=3") == 
+      assert { Predicate.from_url_fragment("a=1&b=2|c=3") == 
         Predicate{ Or( And(Eq("a","1"),Eq("b","2")), Eq("c","3") ) } }
     end
     
     test "parens change precedence" do
-      assert { @converter.convert("a=1|b=2&c=3") == 
+      assert { Predicate.from_url_fragment("a=1|b=2&c=3") == 
         Predicate{ Or( Eq("a","1"), And(Eq("b","2"),Eq("c","3")) ) } }
 
-      assert { @converter.convert("(a=1|b=2)&c=3") == 
+      assert { Predicate.from_url_fragment("(a=1|b=2)&c=3") == 
         Predicate{ And( Or(Eq("a","1"),Eq("b","2")), Eq("c","3") ) } }
     end
 
