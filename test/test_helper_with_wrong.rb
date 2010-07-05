0c5c4cca2627091df80a13f76d051348fbcc2a9d
Steve Conover <sconover@gmail.com>
2010-07-04 17:52:31 -0700

    reflecting changes in new wrong org

--- test/test_helper_with_wrong.rb
+++ test/test_helper_with_wrong.rb
@@ -1,4 +1,5 @@
 require "test/test_helper"
 require "wrong"
-require "wrong/minitest"
-require "wrong/include_test_context"
+require "wrong/adapters/minitest"
+require "wrong/message/test_context"
+require "wrong/message/string_diff"
